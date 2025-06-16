5cf9cc3b07504a10a65c0279ef782489e3005faa
Tengku71 <117705448+Tengku71@users.noreply.github.com>
2025-06-17 06:40:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,14 +42,12 @@ client = MongoClient(
 mongo = client['MyProject']
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'ezzdarkrap2@gmail.com'
-mail = Mail(app)
-
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 # Flask-Login configuration
 login_manager = LoginManager()
 login_manager.init_app(app)
